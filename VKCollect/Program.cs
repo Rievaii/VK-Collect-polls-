5938f2f3570fbc062e7ac0487cc513b0e8927d04
Rievaii <rievaii@yandex.ru>
2022-09-13 17:54:37 +0300

    V.0
    Compiled

---
+++ VKCollect/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace std
+{
+    class Core
+    { 
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello worlds");
+        }
+    }
+
+}
